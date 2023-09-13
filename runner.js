923e16e9204c299a0c44a81d39827d81212bd838
Yatharth Agarwal <yatharth.fractal@gmail.com>
2023-09-13 10:32:46 +0530

    Changed name to anvil

--- runner.js
+++ runner.js
@@ -1,7 +1,7 @@
 const config = require('./config');
 const exportStats = require('./jitsi_prometheus');
 
-const domain = "https://meet-uat.lybl.com/jitsi-meeting-lybl-";
+const domain = "https://meet-uat.lybl.com/anvil-";
 for (let i=0;i<config.numberOfConferences;i++) {
     let url = domain + i;
     console.log(url);
